ee548e0444f430a5e1962aaefa5730009b12f6aa
Gustavo rodrigues <gustavo.katia.rodrigues@gmail.com>
2025-01-26 01:33:19 -0400

    initial add server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+import { config } from "dotenv";
+import express from "express";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
